41088a6bb88767942981f122f0e878efcd519d46
Ali Donyaie <ali@nativeblocks.io>
2024-11-01 16:24:29 +0330

    base format

--- Package.swift
+++ Package.swift
@@ -5,15 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "NativeblocksFoundation",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NativeblocksFoundation",
             targets: ["NativeblocksFoundation"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NativeblocksFoundation"),
         .testTarget(
